c97e17b30fc9e61f8c09ccbd6f75f42194f603e9
manikanta-72 <mani.4272@gmail.com>
2022-11-29 09:54:45 -0800

    multi emotions changes

--- train.py
+++ train.py
@@ -28,11 +28,13 @@ import json
 from json import JSONEncoder
 import numpy
 
+emotion_label = {'neutral':0, 'joy':1, 'angry':2, 'disgust':3, 'suprise':4}
+
 def generate_unique_signals(num_signal, size):
     signals = {}
     signals_np = {}
     for i in range(num_signal):
-        signals[i] = 0.1 * torch.rand(size)
+        signals[i] = 0.01 * torch.rand(size)
         signals_np[i] = signals[i].cpu().numpy()
     return signals, signals_np
 
@@ -53,7 +55,7 @@ if __name__ == '__main__':
     visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
     total_iters = 0                # the total number of training iterations
 
-    signals, signals_np = generate_unique_signals(10, (1,1,28,28))
+    signals, signals_np = generate_unique_signals(5, (1,3,64,64))
     out_file = open("signals.json", "w")
     json.dump(signals_np, out_file, indent = 6,cls=NumpyArrayEncoder)
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
@@ -68,11 +70,12 @@ if __name__ == '__main__':
                 t_data = iter_start_time - iter_data_time
             #print("A_path:", data['A_paths'])
             #print("B_path:", data['B_paths'])
-            label_a = int(data['A_paths'][0].split('/')[-1].split('_')[0])
-            label_b = int(data['B_paths'][0].split('/')[-1].split('_')[0])
+            #label_a = int(data['A_paths'][0].split('/')[-1].split('_')[0])
+            #label_b = int(data['B_paths'][0].split('/')[-1].split('_')[0])
             #print(data['A'].shape)
-            data['A'] += signals[label_b]
-            data['B'] += signals[label_a]
+            tag_b = data['B_paths'][0].split('/')[-1].split('_')[1]
+            data['A'] += signals[emotion_label[tag_b]]
+            data['B'] += signals[0]
             total_iters += opt.batch_size
             epoch_iter += opt.batch_size
             model.set_input(data)         # unpack data from dataset and apply preprocessing
